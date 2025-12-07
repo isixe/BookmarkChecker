10dd1259cb7c9caba96679ac453163b69ad8400b
isixe <isixe@outlook.com>
2025-12-07 23:15:24 +0800

    🎈 perf(app): add favicon link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -52,9 +52,8 @@ export default function RootLayout({
 	return (
 		<html lang="en" suppressHydrationWarning>
 			<head>
-				<link rel="preconnect" href="https://fonts.googleapis.com" />
-				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-				<link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link rel="icon" href="/favicon.ico" />
 			</head>
 			<body className="font-body antialiased">{children}</body>
 		</html>
